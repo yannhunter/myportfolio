3e90555d36ec1fd54b6822c19b8a3b68980f93e2
yannhunter <ekongue@gmail.com>
2024-04-23 02:41:09 +0200

    update menu  in portfolio section

--- app.js
+++ app.js
@@ -128,3 +128,26 @@ modalClose.forEach(e1 => {
         modalViews.forEach(modalView => {modalView.classList.remove('active-modal')})
     })
 })
+
+
+//  intersectionobserver portfolio resume
+const ratio = 0.1
+const options = {
+    root: null,
+    rootMargin: "0px",
+    threshold: ratio,
+  };
+  const  handleIntersect = (entries, observer) => {
+    entries.forEach((entry) => {
+        if(entry.intersectionRatio > ratio){
+            entry.target.classList.add ('reveal-visible')
+            observer.unobserve(entry.target)
+        }
+        
+    })
+}
+  
+  const observer = new IntersectionObserver(handleIntersect, options);
+  document.querySelectorAll('.reveal').forEach((r)=>{
+    observer.observe(r)
+  })
