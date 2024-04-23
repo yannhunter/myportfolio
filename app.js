e472869a538ce4e97857ba082ad5bf335bc68154
yannhunter <ekongue@gmail.com>
2024-04-24 00:55:58 +0200

    update form contact and add slow motion

--- app.js
+++ app.js
@@ -130,24 +130,3 @@ modalClose.forEach(e1 => {
 })
 
 
-//  intersectionobserver portfolio resume
-const ratio = 0.1
-const options = {
-    root: null,
-    rootMargin: "0px",
-    threshold: ratio,
-  };
-  const  handleIntersect = (entries, observer) => {
-    entries.forEach((entry) => {
-        if(entry.intersectionRatio > ratio){
-            entry.target.classList.add ('reveal-visible')
-            observer.unobserve(entry.target)
-        }
-        
-    })
-}
-  
-  const observer = new IntersectionObserver(handleIntersect, options);
-  document.querySelectorAll('.reveal').forEach((r)=>{
-    observer.observe(r)
-  })
